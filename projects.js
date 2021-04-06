cf5da5e9e7ccd02a2b89ba8b6598f050a9888c03
pramza427 <49450701+pramza427@users.noreply.github.com>
2021-04-05 21:51:25 -0500

    cards added

---
+++ projects.js
@@ -0,0 +1,58 @@
+var projects = 
+[
+	{
+		"Name": "Dart-Games",
+		"Description": "Scoreboards for various Dart-Board games.",
+		"Tech": "HTML, JavaScript",
+		"Repo": "https://github.com/pramza427/Dart-Games",
+		"Page": "https://pramza427.github.io/Dart-Games/501.html",
+		"Image": "images/501.jpg",
+		"Display": "True"
+	},
+	{
+		"Name": "Dart-Games",
+		"Description": "Scoreboards for various Dart-Board games.",
+		"Tech": "HTML, JavaScript",
+		"Repo": "https://github.com/pramza427/Dart-Games",
+		"Page": "https://pramza427.github.io/Dart-Games/501.html",
+		"Image": "images/501.jpg",
+		"Display": "True"
+	},
+	{
+		"Name": "Dart-Games",
+		"Description": "Scoreboards for various Dart-Board games.",
+		"Tech": "HTML, JavaScript",
+		"Repo": "https://github.com/pramza427/Dart-Games",
+		"Page": "https://pramza427.github.io/Dart-Games/501.html",
+		"Image": "images/501.jpg",
+		"Display": "True"
+	},
+	{
+		"Name": "Dart-Games",
+		"Description": "Scoreboards for various Dart-Board games.",
+		"Tech": "HTML, JavaScript",
+		"Repo": "https://github.com/pramza427/Dart-Games",
+		"Page": "https://pramza427.github.io/Dart-Games/501.html",
+		"Image": "images/501.jpg",
+		"Display": "True"
+	},
+	{
+		"Name": "Dart-Games",
+		"Description": "Scoreboards for various Dart-Board games.",
+		"Tech": "HTML, JavaScript",
+		"Repo": "https://github.com/pramza427/Dart-Games",
+		"Page": "https://pramza427.github.io/Dart-Games/501.html",
+		"Image": "images/501.jpg",
+		"Display": "True"
+	},
+	{
+		"Name": "Dart-Games",
+		"Description": "Scoreboards for various Dart-Board games.",
+		"Tech": "HTML, JavaScript",
+		"Repo": "https://github.com/pramza427/Dart-Games",
+		"Page": "https://pramza427.github.io/Dart-Games/501.html",
+		"Image": "images/501.jpg",
+		"Display": "True"
+	}
+
+];

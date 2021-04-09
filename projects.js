3173550a56d181a6322be834b47e6db4bb53e88d
pramza427 <pramza427@gmail.com>
2021-04-08 19:59:14 -0500

    Added project to project list

--- projects.js
+++ projects.js
@@ -6,7 +6,25 @@ var projects =
 		"Tech": "HTML, JavaScript",
 		"Repo": "https://github.com/pramza427/Dart-Games",
 		"Page": "https://pramza427.github.io/Dart-Games/501.html",
-		"Image": "/images/501.jpg",
+		"Image": "images/501.jpg",
+		"Display": "True"
+	},
+	{
+		"Name": "Highway Rush",
+		"Description": "A sidescroller where you race down the highway avoiding other cars and picking up gas to try and get the highest score",
+		"Tech": "HTML, JavaScript",
+		"Repo": "https://github.com/pramza427/IT202-Spring2021-project2",
+		"Page": "https://pramza427.github.io/IT202-Spring2021-project2",
+		"Image": "images/HighwayRush.jpg",
+		"Display": "True"
+	},
+	{
+		"Name": "Dart-Games",
+		"Description": "Scoreboards for various Dart-Board games.",
+		"Tech": "HTML, JavaScript",
+		"Repo": "https://github.com/pramza427/IT202-Spring2021-project2",
+		"Page": "https://pramza427.github.io/IT202-Spring2021-project2",
+		"Image": "images/501.jpg",
 		"Display": "True"
 	},
 	{
@@ -15,7 +33,7 @@ var projects =
 		"Tech": "HTML, JavaScript",
 		"Repo": "https://github.com/pramza427/Dart-Games",
 		"Page": "https://pramza427.github.io/Dart-Games/501.html",
-		"Image": "/images/501.jpg",
+		"Image": "images/501.jpg",
 		"Display": "True"
 	},
 	{
@@ -24,7 +42,7 @@ var projects =
 		"Tech": "HTML, JavaScript",
 		"Repo": "https://github.com/pramza427/Dart-Games",
 		"Page": "https://pramza427.github.io/Dart-Games/501.html",
-		"Image": "/images/501.jpg",
+		"Image": "images/501.jpg",
 		"Display": "True"
 	},
 	{
@@ -33,25 +51,7 @@ var projects =
 		"Tech": "HTML, JavaScript",
 		"Repo": "https://github.com/pramza427/Dart-Games",
 		"Page": "https://pramza427.github.io/Dart-Games/501.html",
-		"Image": "/images/501.jpg",
-		"Display": "True"
-	},
-	{
-		"Name": "Dart-Games",
-		"Description": "Scoreboards for various Dart-Board games.",
-		"Tech": "HTML, JavaScript",
-		"Repo": "https://github.com/pramza427/Dart-Games",
-		"Page": "https://pramza427.github.io/Dart-Games/501.html",
-		"Image": "/images/501.jpg",
-		"Display": "True"
-	},
-	{
-		"Name": "Dart-Games",
-		"Description": "Scoreboards for various Dart-Board games.",
-		"Tech": "HTML, JavaScript",
-		"Repo": "https://github.com/pramza427/Dart-Games",
-		"Page": "https://pramza427.github.io/Dart-Games/501.html",
-		"Image": "/images/501.jpg",
+		"Image": "images/501.jpg",
 		"Display": "True"
 	}
 

2a57a786b075b3ef8c9cdae5fccf661d33350810
pramza427 <pramza427@gmail.com>
2021-05-12 18:38:19 -0500

    Add projects, down arrow

--- projects.js
+++ projects.js
@@ -2,7 +2,7 @@ var projects =
 [
 	{
 		"Name": "Dart-Games",
-		"Description": "Scoreboards for various Dart-Board games.",
+		"Description": "A quick and easy way for users to keep track of scores in dart games",
 		"Tech": "HTML, JavaScript",
 		"Repo": "https://github.com/pramza427/Dart-Games",
 		"Page": "https://pramza427.github.io/Dart-Games/501.html",
@@ -19,12 +19,21 @@ var projects =
 		"Display": "True"
 	},
 	{
-		"Name": "Dart-Games",
-		"Description": "Scoreboards for various Dart-Board games.",
+		"Name": "Bike Helper",
+		"Description": "Filters and displays biking incidents and bike racks around Chicago",
 		"Tech": "HTML, JavaScript",
-		"Repo": "https://github.com/pramza427/IT202-Spring2021-project2",
-		"Page": "https://pramza427.github.io/IT202-Spring2021-project2",
-		"Image": "images/501.jpg",
+		"Repo": "https://github.com/pramza427/IT202-Spring2021-BigProject",
+		"Page": "https://pramza427.github.io/IT202-Spring2021-BigProject/#",
+		"Image": "images/BikeHelper.jpg",
+		"Display": "True"
+	},
+	{
+		"Name": "Recipe Finder",
+		"Description": "Uses a recipe API to quickly and easily filter through recipes and display them to the user",
+		"Tech": "HTML, JavaScript",
+		"Repo": "https://github.com/pramza427/RecipeFinder",
+		"Page": "https://pramza427.github.io/RecipeFinder/index.html",
+		"Image": "images/RecipeFinder.jpg",
 		"Display": "True"
 	},
 	{
@@ -34,7 +43,7 @@ var projects =
 		"Repo": "https://github.com/pramza427/Dart-Games",
 		"Page": "https://pramza427.github.io/Dart-Games/501.html",
 		"Image": "images/501.jpg",
-		"Display": "True"
+		"Display": "False"
 	},
 	{
 		"Name": "Dart-Games",
@@ -43,16 +52,7 @@ var projects =
 		"Repo": "https://github.com/pramza427/Dart-Games",
 		"Page": "https://pramza427.github.io/Dart-Games/501.html",
 		"Image": "images/501.jpg",
-		"Display": "True"
-	},
-	{
-		"Name": "Dart-Games",
-		"Description": "Scoreboards for various Dart-Board games.",
-		"Tech": "HTML, JavaScript",
-		"Repo": "https://github.com/pramza427/Dart-Games",
-		"Page": "https://pramza427.github.io/Dart-Games/501.html",
-		"Image": "images/501.jpg",
-		"Display": "True"
+		"Display": "False"
 	}
 
 ];
